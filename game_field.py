ee6c38e89ea61520fce4ba75a802aeb8940c29c9
adipythony <adiissan415@gmail.com>
2023-09-18 11:55:31 +0300

    change

--- game_field.py
+++ game_field.py
@@ -1,6 +1,10 @@
-for row in range(amount_of_days):
-    row_list = []
-    for col in range(hour_per_day):
-        row_list.append('free')
-    schedule.append(row_list)
-list_print(schedule)
+import consts
+def build_game_board(game_board):
+    for row in range(25):
+        row_list = []
+        for col in range(50):
+            row_list.append('')
+        game_board.append(row_list)
+    print(game_board)
+    return game_board
+build_game_board(consts.game_board)
