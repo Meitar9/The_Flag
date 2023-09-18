dbf74bd7389c9e074895139c81273ccb2ea1033a
Meitar <meitarshamir9@gmail.com>
2023-09-18 14:37:32 +0300

    Add vision board

--- game_field.py
+++ game_field.py
@@ -1,11 +1,26 @@
 import consts
+import pygame
+import screen
+
+
 def build_game_board(game_board):
     for row in range(25):
         row_list = []
         for col in range(50):
-            row_list.append('')
+            row_list.append(draw_rect(row, col))
         game_board.append(row_list)
-    print(game_board)
-    return game_board
+
+    while True:
+        pygame.init()
+
+def draw_rect(row, col):
+    pygame.draw.rect(surface=screen.screen, color=consts.rect_color, rect=((place_rect(row, col)), (30, 30)), width=1)
+    pygame.display.flip()
+
+def place_rect(row, col):
+    rect_y = 30 * row
+    rect_x = 30 * col
+    return rect_x, rect_y
+
 
 build_game_board(consts.game_board)
