ff3fb0d374e46bbb62a5e940065cd6877715418c
Meitar <meitarshamir9@gmail.com>
2023-09-18 12:03:48 +0300

    Add screen close

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ import screen
 state = {"running": True}
 def open_screen():
     while state["running"]:
+        close()
         pygame.init()
         screen.green()
 
+
+def close():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            state["running"] = False
 
 open_screen()
